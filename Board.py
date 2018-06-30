96bfdc2defd6661c1c2dde39b98da4a24b27f58d
Dylan Snyer <dylansnyder31@gmail.com>
2018-06-30 09:56:33 -0400

    Added function for the pressing and releasing of each piece

--- Board.py
+++ Board.py
@@ -1,41 +1,30 @@
-
 from kivy.uix.screenmanager import Screen
-from kivy.config import Config
 from kivy.core.window import Window
-from kivy.factory import Factory
-from kivy.uix.gridlayout import GridLayout
-from kivy.lang import Builder
-from kivy.uix.button import Button
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.image import Image
-from kivy.uix.floatlayout import FloatLayout
 
-
+from position_of_mouse import find_position
 
 class board_setup(Screen):
-    pass
+
+    def on_mouse_pos(self, pos):
+        #This function gets the position of the mouse, in chessboard labels
+        position = find_position()
+        print(position.chess_position(pos))
 
 
+    def touched(self, id):
+        print(id)
+
+
+    def release(self):
+        print("RRR")
+
+
+
+    Window.bind(mouse_pos = on_mouse_pos)
+
+
+#Builds the App
 class window(App):
     def build(self):
         return board_setup()
-
-
-if __name__ == "__main__":
-    width_of_board = 820
-    height_of_board = 800
-
-    #Set the Hight and Width of the App
-    Config.set('graphics', 'width', str(width_of_board))
-    Config.set('graphics', 'height', str(height_of_board))
-
-    #Make the App non-resizable
-    Config.set('graphics', 'resizable', '0')
-    Config.write()
-
-    #Make the top windows bar go away
-    Window.borderless = True
-
-    #Runs the
-    window().run()
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from kivy.app import App
+from Board import window
+from kivy.config import Config
+from kivy.core.window import Window
+
+def setup():
+    width_of_board = 820
+    height_of_board = 800
+
+    #Set the Hight and Width of the App
+    Config.set('graphics', 'width', str(width_of_board))
+    Config.set('graphics', 'height', str(height_of_board))
+
+    #Make the App non-resizable
+    Config.set('graphics', 'resizable', '0')
+    Config.write()
+
+    #Make the top windows bar go away
+    Window.borderless = True
+
+    #Runs the
+    window().run()
 
 
-
-
+if __name__ == "__main__":
+    setup()
 
 '''
 TODO LIST
 
-1. FIGURE OUT CODE ARCHITECTURE (HARD)
-2. DISPLAY THE PIECES AND MAKE IT LOOK SEEMLESS (easy when code arch. is finished)
+
+
 2.5 CODE A DRAGGING SYSTEM AND A TOUCH-BASED SYSTEM
     2.75 MAKE THE DRAGGING SYSTEM HAVE THE PIECES MOVE A LAYER UP FROM THE BOARD
 3. CODE ALL OF THE MOVES FOR THE PIECES (HARD; MAKE CODE STILL NEAT)
