6a90e0b3942c02bac50a404388ae220291ac31a2
Dylan Snyer <dylansnyder31@gmail.com>
2018-07-01 14:36:59 -0400

    Figured out how to get duel functionality

--- Board.py
+++ Board.py
@@ -8,9 +8,12 @@ class Scatter_Text_widget(FloatLayout):
 
 
     def on_touch_down(self, touch):
-        position = find_position()
-        pos_chess = position.chess_position(touch.pos)
-        print(pos_chess)
+        res = super(Scatter_Text_widget, self).on_touch_down(touch)
+        if res:
+            position = find_position()
+            pos_chess = position.chess_position(touch.pos)
+            print(pos_chess)
+        return res
 
     def on_touch_up(self, touch):
         position = find_position()
