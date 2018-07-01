1cce6e16f1191605bb343e353a159ffb70deb91b
Dylan Snyer <dylansnyder31@gmail.com>
2018-07-01 08:40:31 -0400

    The mouse and the pieces are now alligned; No pieces have specified movements yet.

--- Board.py
+++ Board.py
@@ -1,13 +1,18 @@
-from kivy.uix.screenmanager import Screen
-from kivy.core.window import Window
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.scatter import Scatter
-from position_of_mouse import find_position
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+
+from position_of_mouse import find_position
 
 class Scatter_Text_widget(FloatLayout):
-    pass
+    def on_mouse_pos(self, pos):
+        #This function gets the position of the mouse, in chessboard labels
+        position = find_position()
+        pos_chess = position.chess_position(pos)
+        print(pos_chess)
+
+
+    Window.bind(mouse_pos = on_mouse_pos)
 
 
 
